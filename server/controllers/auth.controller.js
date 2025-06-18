7884c4ec2427791d6b8ee5c7dd30e56da592d7d0
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-18 23:31:05 +0530

    Created basic Routes and UI and Connected to the DB

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+export const getSignupPage = (req, res) => {
+  try {
+    return res.status(200).render("signup");
+  } catch (err) {
+    return res.status(404).send("Page Not Found");
+  }
+};
+
+export const getLoginPage = (req, res) => {
+  try {
+    return res.status(200).render("login");
+  } catch (err) {
+    return res.status(404).send("Page Not Found");
+  }
+};
+
+export const signup = async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(404).send("Page Not Found");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+  } catch (err) {
+    return res.status(404).send("Page Not Found");
+  }
+};
